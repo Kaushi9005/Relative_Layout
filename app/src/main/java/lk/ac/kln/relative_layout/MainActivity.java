2c1a52841b21d951be931512928b5ada92535ba9
Kaushi9005 <kaushalyanadeeshani96@gmail.com>
2019-04-01 09:44:22 +0530

    Initial commit

---
+++ app/src/main/java/lk/ac/kln/relative_layout/MainActivity.java
@@ -0,0 +1,27 @@
+package lk.ac.kln.relative_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+
+    }
+
+    public void buttonOneClick(View view){
+        editText.setText("1");
+    }
+
+    public void buttonTwoClick(View view) {
+        editText.setText("2");
+    }
+}
